5288a62e3ede8be2a7c79dead43babf8416ebf1c
Rart3x <ramjattankeny971@gmail.com>
2024-06-13 13:37:33 +0200

    Spacing function for args and new conditions for case like (FF D')

--- Utils.py
+++ Utils.py
@@ -7,6 +7,16 @@ def is_in_set(arg):
             return False
     return True
 
+
+def is_spaced(args):
+    '''IsSpaced function'''
+
+    for arg in args:
+        if len(arg) > 2:
+            return False
+    return True
+
+
 def check_splitted_args(args):
     '''CheckSplitted function'''
 
@@ -14,23 +24,33 @@ def check_splitted_args(args):
     numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
 
     for arg in args:
+        prev_char = None
         for i, char in enumerate(arg):
             if i == 0 and (char in numbers or char == "'"):
                 return False
             if i!= 0 and (char in numbers or char == "'") and arg[i - 1] not in letters:
                 return False
+            if prev_char in letters and char in letters:
+                return False
+            prev_char = char
+
     return True
+
 
 def check_args_validity(ac, av):
     '''CheckArgs validity'''
 
     if ac!= 2:
         raise ValueError("Error: Invalid number of arguments")
+    
     if not is_in_set(av[1]):
         raise ValueError("Error: Invalid caracter in arguments")
 
     args = av[1].split()
+
     if not check_splitted_args(args):
         raise ValueError("Error: Invalid pattern in arguments")
+    if not is_spaced(args):
+        raise ValueError("Error: Invalid spacing in arguments")
 
     return args
