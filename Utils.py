b073be7d93c6009910d3ed4589d6f55b2ba0ac5a
Rart3x <ramjattankeny971@gmail.com>
2024-06-13 13:22:03 +0200

    New life in Python

---
+++ Utils.py
@@ -0,0 +1,36 @@
+def is_in_set(arg):
+    '''IsInSet function'''
+
+    set_chars = {'F', 'R', 'U', 'B', 'L', 'D', '2', "'", ' '}
+    for char in arg:
+        if char not in set_chars:
+            return False
+    return True
+
+def check_splitted_args(args):
+    '''CheckSplitted function'''
+
+    letters = {'F', 'R', 'U', 'B', 'L', 'D'}
+    numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+
+    for arg in args:
+        for i, char in enumerate(arg):
+            if i == 0 and (char in numbers or char == "'"):
+                return False
+            if i!= 0 and (char in numbers or char == "'") and arg[i - 1] not in letters:
+                return False
+    return True
+
+def check_args_validity(ac, av):
+    '''CheckArgs validity'''
+
+    if ac!= 2:
+        raise ValueError("Error: Invalid number of arguments")
+    if not is_in_set(av[1]):
+        raise ValueError("Error: Invalid caracter in arguments")
+
+    args = av[1].split()
+    if not check_splitted_args(args):
+        raise ValueError("Error: Invalid pattern in arguments")
+
+    return args
---
+++ main.py
@@ -0,0 +1,21 @@
+#!
+
+import pygame
+import sys
+
+from Rubik import *
+from Utils import *
+
+def main() -> int:
+    '''Main function'''
+
+    rubik = Rubik()
+
+    try:
+        rubik.set_args(check_args_validity(len(sys.argv), sys.argv))
+    except ValueError as e:
+        print("\033[31m", e, "\033[30m")
+
+
+if __name__ == "__main__":
+    main()
