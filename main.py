5cd35a315c9184aaa63043eb110e45fa0399b165
Rart3x <ramjattankeny971@gmail.com>
2024-06-15 07:39:28 +0200

    UnDeleted Rubik.py, and ReAdd set/get_args()

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 from Display import *
+from Rubik import *
 from Utils import *
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
@@ -12,12 +13,15 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 def main() -> int:
     '''Main function'''
 
+    rubik = Rubik()
+
     if os.path.exists("./models_compressed/"):
         os.remove("./models_compressed/cube.bam")
         os.rmdir("./models_compressed")
 
     try:
-        display(check_args_validity(len(sys.argv), sys.argv))
+        rubik.set_args(check_args_validity(len(sys.argv), sys.argv))
+        display(rubik.get_args())
     except ValueError as e:
         print("\033[31m", e, "\033[30m")
 
