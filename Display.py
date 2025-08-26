42b3920f96b2ac3e94df5dce24a91eb08355c3d8
Rart3x <ramjattankeny971@gmail.com>
2025-08-26 17:30:15 +0200

    feat: clean

--- Display.py
+++ Display.py
@@ -80,28 +80,25 @@ def automatic_input(args):
         if len(i) > 1 and not i[1].isdigit() and not in_animation:
             
             in_animation = True
-
             axis, layer, angle = rot_dict[i[0].lower() + i[1]]
+            
             apply_movement(axis, layer)
-            
             animate_rotation(center, axis, angle, duration)
-            
             invoke(lambda: process_next_input(index), delay=duration + duration / 2)
         
         elif not in_animation:
             
             in_animation = True
-            
             axis, layer, angle = rot_dict[i[0].lower()]
+            
             apply_movement(axis, layer)
-            
             animate_rotation(center, axis, angle, duration)
-            
             invoke(lambda: process_next_input(index), delay=duration + duration / 2)
 
     def process_next_input(prev_index):
         
         global in_animation
+        
         in_animation = False
         
         process_input(prev_index + 1)
@@ -112,10 +109,9 @@ def automatic_input(args):
 def display(rubik: Rubik):
     '''Display 3D Rubik's Cube, manipulating Blender Object'''
 
-    rubik.set_mixed_cube(init())
-
     global args_g, app
 
+    rubik.set_mixed_cube(init())
     args_g = rubik.get_args()
 
     for position in product((-1, 0, 1), repeat=3):
@@ -138,9 +134,9 @@ def init():
 
     app = Ursina(development_mode=False, title="Rubik")
     center = Entity()
-    root = tkinter.Tk()
-
+    inputs = generate_input(10)
     nbr_field = InputField(y=-.35, limit_content_to='0123456789', active=True)
+    root = tkinter.Tk()
 
     # Create a button to submit the number of movements to mix the cube
     Button(text='Mixing', scale=.1, color=color.cyan.tint(-.4), x=0.30, y=-.35, on_click=submit).fit_to_text()
@@ -151,8 +147,6 @@ def init():
     window.position = (w / 4, h / 4)
 
     EditorCamera()
-
-    inputs = generate_input(10)
 
     return inputs
 
@@ -170,14 +164,12 @@ def input(key):
 
     # Navigate on normal/reverse movements with keyboard arrows
     if held_keys["left arrow"]:
-        print("left")
         if len(r_seq) > 0 and idx - 1 >= -1:
             if len(r_seq) > idx >= 0:
                 animation_sequence(r_seq[idx])
                 idx -= 1
 
     if held_keys["right arrow"] and len(seq) > 0:
-        print("right")
         if idx < len(seq) - 1:
             idx += 1
             animation_sequence(seq[idx])
@@ -201,9 +193,11 @@ def input(key):
         if args_g is not None:
             if idx < len(seq) - 1:
                 seq = insert_and_shift_arr(seq, idx, args_g)
+            
             decompose_arr_args(args_g, seq)
             reverse_seq_update()
             automatic_input(args_g)
+            
             idx = len(seq) - 1
 
     if key not in rot_dict:
@@ -229,6 +223,8 @@ def input(key):
 
 
 def animate_rotation(center, axis, angle, duration):
+    """Animate rotation function"""
+
     if axis == 'x':
         center.animate('rotation_x', angle, duration=duration)
     elif axis == 'y':
@@ -238,7 +234,8 @@ def animate_rotation(center, axis, angle, duration):
 
 
 def end_animation():
-    
+    """End animation function"""
+
     global in_animation
     in_animation = False
 
@@ -259,14 +256,16 @@ def submit():
     global idx, seq
 
     input_text = nbr_field.text
+    
     if not input_text.isdigit():
         return
+    
     input_integer = int(input_text)
-
     nbr_field.text = ""
 
     if input_integer > 1000:
         return
+    
     inputs = generate_input(input_integer)
     inputs = expand_double_inputs(inputs)
     
